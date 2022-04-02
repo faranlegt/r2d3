d119b7a4a9f32f6a9ecde01289ddcebf411312bb
Yegor Zhumikov <yegor@yogurtthehor.se>
2022-04-02 18:17:33 +0300

    feat: better movement

---
+++ Assets/Scripts/Core/EffectsManager.cs
@@ -0,0 +1,15 @@
+using System;
+using UnityEngine;
+
+namespace Ld50.Core
+{
+    public class EffectsManager : MonoBehaviour
+    {
+        public Camera playerCamera;
+
+        private void Update()
+        {
+            
+        }
+    }
+}
--- Assets/Scripts/Core/PlayerMovement.cs
+++ Assets/Scripts/Core/PlayerMovement.cs
@@ -1,4 +1,5 @@
 using System;
+using UniRx;
 using UnityEngine;
 
 namespace Ld50.Core
@@ -11,6 +12,7 @@ namespace Ld50.Core
 
         private Player _player;
         private Rigidbody2D _rigidbody;
+        private Vector2 _direction;
 
         private void Awake()
         {
@@ -21,26 +23,31 @@ namespace Ld50.Core
         public void Update()
         {
             var directionRounded = Direction.None;
-            var direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+            _direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
-            if (direction.sqrMagnitude > 0.1)
+            if (_direction.sqrMagnitude > 0.1)
             {
-                var angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+                var angle = Mathf.Atan2(_direction.x, _direction.y) * Mathf.Rad2Deg;
 
                 if (angle < 0)
                     angle += 360f;
 
 
                 directionRounded = (Direction)Mathf.FloorToInt(angle / 90f);
-
-                _rigidbody.velocity = direction.normalized * speed;
             }
             else
             {
-                _rigidbody.velocity = Vector2.zero;
+                _direction = Vector2.zero;
             }
 
             _player.SetDirection(directionRounded);
+        }
+
+        private void FixedUpdate()
+        {
+            if (_direction.sqrMagnitude < 0.1f) return;
+            
+            _rigidbody.MovePosition(_rigidbody.position + _direction.normalized * speed * Time.fixedDeltaTime); 
         }
     }
 }
