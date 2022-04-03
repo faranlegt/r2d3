aa43f5124febe7f759e662c56f6f997719c3ceeb
Yegor Zhumikov <yegor@yogurtthehor.se>
2022-04-03 15:34:24 +0300

    feat: add holes

--- Assets/Scripts/Interactable/Hole.cs
+++ Assets/Scripts/Interactable/Hole.cs
@@ -1,9 +1,45 @@
+using System;
 using UnityEngine;
 
 namespace Ld50.Interactable
 {
     public class Hole : MonoBehaviour
     {
+        public Sprite repaired;
+
+        public bool isBroken;
+
+        public SpriteRenderer tapeRenderer;
         
+        private SpriteRenderer _renderer;
+
+        private void Awake()
+        {
+            _renderer = GetComponent<SpriteRenderer>();
+
+            _renderer.enabled = false;
+            tapeRenderer.enabled = false;
+        }
+
+        private void Update()
+        {
+            if (isBroken)
+            {
+                // todo: brake ship continuously
+            }
+        }
+
+        public void Activate()
+        {
+            _renderer.enabled = true;
+            isBroken = true;
+        }
+
+        public void Repair()
+        {
+            isBroken = false;
+            _renderer.sprite = repaired;
+            tapeRenderer.enabled = true;
+        }
     }
 }
--- Assets/Scripts/Transports/WelderAction.cs
+++ Assets/Scripts/Transports/WelderAction.cs
@@ -1,5 +1,10 @@
+using System;
+using System.Collections.Generic;
 using Ld50.Animations;
 using Ld50.Characters;
+using Ld50.Interactable;
+using UniRx;
+using UniRx.Triggers;
 using UnityEngine;
 
 namespace Ld50.Transports
@@ -7,17 +12,62 @@ namespace Ld50.Transports
     public class WelderAction : MonoBehaviour, ITransportAction
     {
         public SpritesLine welding;
+
+        public List<Hole> holes = new();
+
+        public float actionTime;
+
+        public float repairTime = 2f;
+        
         private Character _character;
         private LineAnimator _animator;
+        private Transport _transport;
 
         private void Awake()
         {
             _animator = GetComponent<LineAnimator>();
             _character = GetComponent<Character>();
+            _transport = GetComponent<Transport>();
+        }
+
+        private void Start()
+        {
+            this.OnTriggerEnter2DAsObservable()
+                .Where(c => c.gameObject.CompareTag("Hole"))
+                .Select(c => c.gameObject.GetComponent<Hole>())
+                .Do(holes.Add)
+                .Subscribe()
+                .AddTo(this);
+            
+            
+            this.OnTriggerExit2DAsObservable()
+                .Where(c => c.gameObject.CompareTag("Hole"))
+                .Select(c => c.gameObject.GetComponent<Hole>())
+                .Do(h => holes.Remove(h))
+                .Subscribe()
+                .AddTo(this);
+        }
+
+        private void Update()
+        {
+            if (!_transport.isActing) return;
+
+            actionTime += Time.deltaTime;
+
+            if (actionTime >= repairTime)
+            {
+                foreach (var hole in holes)
+                {
+                    hole.Repair();
+                }
+                
+                holes.Clear();
+            }
         }
 
         public void StartAction()
         {
+            actionTime = 0;
             _animator.StartLine(welding, loop: true);
         }
 
