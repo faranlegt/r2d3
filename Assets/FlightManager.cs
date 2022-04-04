f29907ac79e5efd48b37b95079ad566e72e2fd27
13x666 <gordeyev.y@gmail.com>
2022-04-04 14:44:02 +0300

    Add basic camera shake

--- Assets/FlightManager.cs
+++ Assets/FlightManager.cs
@@ -6,8 +6,6 @@ namespace Ld50
 {
     public class FlightManager : MonoBehaviour
     {
-        bool _started = false;
-
         public GameObject space;
         private float noiseT = 0f;
         private Vector3 spaceStartPos;
@@ -15,21 +13,17 @@ namespace Ld50
         public ParticleSystem psFlow;
 
         public Camera mainCamera;
+        private Core.CameraFollower _cameraFollower;
 
         void Start()
         {
             spaceStartPos = space.transform.position;
+            _cameraFollower = mainCamera.GetComponent<Core.CameraFollower>();
+            psFlow.Play();
         }
 
         void Update()
         {
-            if (!_started)
-            {
-                psFlow.Play();
-                _started = true;
-            }
-
-
             noiseT += 0.001f;
 
             var noise1 = Mathf.PerlinNoise(noiseT, 0) - 0.5f;
@@ -44,7 +38,15 @@ namespace Ld50
             space.transform.Rotate(0, 0, noise1 * 0.5f);
             psFlow.transform.rotation = Quaternion.Euler(0, 0, noise1 * 50f);
 
+            // CAMERA
+
             mainCamera.transform.rotation = Quaternion.Euler(0, 0, noise1 * 10f);
+
+            var camOffsetX = (Mathf.PerlinNoise(noiseT * 7, 0) - 0.5f) * 0.5f;
+            var camOffsetY = (Mathf.PerlinNoise(0, noiseT * 7) - 0.5f) * 0.5f;
+
+            _cameraFollower.offset = new Vector3(camOffsetX, camOffsetY, -10);
+            
         }
     }
 }
